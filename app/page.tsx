6611dcaa12a88ec5a14f5b665a73774faf86bfac
kims1998 <seung.kim1998@gmail.com>
2025-11-15 17:28:25 -0800

    removed leaderboard and included reward options and total reward points

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Layout from "./components/Layout";
 
-//Screen Dimension on Chrome Inspect Element is 320px x 568px
-
 export default function Home() {
   return (
     <Layout>
-      <div className="flex justify-center pt-4">
+      <div className="flex flex-col items-center gap-6 pt-4">
+      {/* Points Card */}
         <div className="w-full max-w-md">
-          <div className="bg-[#ECECEC] rounded-xl p-8 sm:p-12 flex flex-col items-center justify-center shadow-sm">
+          <div className="home-container bg-[#ECECEC]">
             <div className="text-center">
               <div className="text-7xl sm:text-8xl font-medium text-black mb-6">
                 0
@@ -17,6 +16,20 @@ export default function Home() {
               </div>
             </div>
           </div>
+      {/* END Points Card */}
+
+      {/* Total Recycled Card */}
+          <div className="home-container bg-[#D31635]">
+            <div className="text-center">
+              <div className="text-7xl sm:text-8xl font-medium text-black mb-6">
+                0
+              </div>
+              <div className="text-sm sm:text-base font-medium text-black">
+                Total Recycled
+              </div>
+            </div>
+          </div>
+        {/* END Total Recycled Card */}
         </div>
       </div>
     </Layout>
